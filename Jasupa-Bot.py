bfceb3a083523c8cc7df6bc4743c434ea3348360
Jasupa <jasper.toirkens@gmail.com>
2019-12-22 18:34:12 +0100

    lmao

--- Jasupa-Bot.py
+++ Jasupa-Bot.py
@@ -6,34 +6,31 @@ from discord.ext import commands #Importeer alle command-commands van Discord
 
 client = commands.Bot(command_prefix = 'ß') #Client variabel maken en prefix zetten
 
-players = {}
-
 #Wat gebeurt er als de bot klaar is met opstarten?
 @client.event
 async def on_ready():
     print("De bot is klaar met opstarten!")
 
-@client.command(pass_context=True)
+@bot.commands(pass_context=True, aliases=['j','joi'])
 async def join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
 
-@client.command(pass_context=True)
-async def leave(ctx):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    await voice_client.disconnect()
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
 
-@client.command(pass_context=True)
-async def play (ctx, url):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    player = await voice_client.create_ytdl_player(url)
-    players[server.id] = player
-    player.start()
-
-
-
+    await voice.disconnect()
+    
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice= await channel.connect():
+        print("De bot is verbonden met {channel}\n")
+    
+    await ctx.send("{channel} gejoined")
 
 
 
