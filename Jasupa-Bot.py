bc49bf9c438dc379e27b661e61bb1595b622770b
Jasupa <jasper.toirkens@gmail.com>
2019-12-22 19:17:13 +0100

    Update Jasupa-Bot.py

--- Jasupa-Bot.py
+++ Jasupa-Bot.py
@@ -1,6 +1,7 @@
 import discord #Importeer alle code van Discord
 import os
 import youtube_dl
+from discord.utils import get
 
 from discord.ext import commands #Importeer alle command-commands van Discord
 
@@ -11,9 +12,9 @@ bot = commands.Bot(command_prefix = 'ß') #Client variabel maken en prefix zette
 async def on_ready():
     print("De bot is klaar met opstarten!")
 
-@bot.command(pass_context=True, aliases=['j','joi'])
+
+@bot.command(pass_context=True, aliases=['j', 'joi'])
 async def join(ctx):
-    global voice
     channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild=ctx.guild)
 
@@ -23,14 +24,73 @@ async def join(ctx):
         voice = await channel.connect()
 
     await voice.disconnect()
-    
+
     if voice and voice.is_connected():
         await voice.move_to(channel)
     else:
         voice = await channel.connect()
-        print("De bot is verbonden met {channel}\n")
-    
-    await ctx.send("{channel} gejoined")
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"Joined {channel}")
+
+
+@bot.command(pass_context=True, aliases=['l', 'lea'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+        await ctx.send(f"Left {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("Don't think I am in a voice channel")
+
+
+@bot.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
 
 
 
